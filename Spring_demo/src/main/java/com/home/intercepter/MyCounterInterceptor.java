3fc19f2ce738163fe1ce46ac52973f0b054107a0
Anuj <Anuj@DESKTOP-O8SM244>
2020-09-26 21:55:49 +0530

    changes

--- Spring_demo/src/main/java/com/home/intercepter/MyCounterInterceptor.java
+++ Spring_demo/src/main/java/com/home/intercepter/MyCounterInterceptor.java
@@ -5,8 +5,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 @Component
 public class MyCounterInterceptor extends HandlerInterceptorAdapter {
@@ -16,9 +16,19 @@ public class MyCounterInterceptor extends HandlerInterceptorAdapter {
   public boolean preHandle (HttpServletRequest request,
                             HttpServletResponse response,
                             Object handler) throws Exception {
-	  System.out.println("ssssssssssssssssssssssss");
+	  System.out.println("Entered preHandle interceptor.");
 
       request.setAttribute("visitorCounter", counter.incrementAndGet());
       return true;
   }
+  
+  @Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,ModelAndView modelAndView) {
+		System.out.println("Entered postHandle interceptor.");
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,Exception exception) {
+		System.out.println("Entered afterCompletion iterceptor");
+	}
 }
